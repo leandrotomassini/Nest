101d25ad1a99d7249650a888654502266ffb3535
Leandro C. Tomassini <leandroctomassini@gmail.com>
2024-03-13 22:01:42 -0300

    Sección 10

---
+++ workspace/v2/04-teslo-shop/src/products/products.service.ts
@@ -0,0 +1,90 @@
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { validate as isUUID } from 'uuid';
+
+import { CreateProductDto } from './dto/create-product.dto';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { Product } from './entities/product.entity';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Injectable()
+export class ProductsService {
+
+  private readonly logger = new Logger( 'ProductsService' );
+
+  constructor(
+    @InjectRepository( Product )
+    private readonly productRepository: Repository<Product>
+  ) { }
+
+  async create( createProductDto: CreateProductDto ) {
+    try {
+      const product = this.productRepository.create( createProductDto );
+      await this.productRepository.save( product );
+      return product;
+    } catch ( error ) {
+      this.handleDbException( error );
+    }
+  }
+
+
+  async findAll( paginationDto: PaginationDto ) {
+    const { limit = 10, offset = 0 } = paginationDto;
+    return this.productRepository.find( {
+      take: limit,
+      skip: offset
+    } );
+  }
+
+  async findOne( term: string ) {
+
+    let product: Product;
+
+    if ( isUUID( term ) ) {
+      product = await this.productRepository.findOneBy( { id: term } );
+    } else {
+      const queryBuilder = this.productRepository.createQueryBuilder();
+
+      product = await queryBuilder
+        .where( 'UPPER(title) =:title or slug =:slug', {
+          title: term.toUpperCase(),
+          slug: term.toLocaleLowerCase()
+        } ).getOne();
+    }
+
+    if ( !product ) {
+      throw new NotFoundException( `Product with term '${ term }' not found.` );
+    }
+
+    return product;
+  }
+
+  async update( id: string, updateProductDto: UpdateProductDto ) {
+    const product = await this.productRepository.preload( {
+      id,
+      ...updateProductDto
+    } );
+
+    if ( !product ) throw new NotFoundException( `Product with id: '${ id }' not found.` );
+
+    try {
+      await this.productRepository.save( product );
+      return product;
+    } catch ( error ) {
+      this.handleDbException( error );
+    }
+  }
+
+  async remove( id: string ) {
+    const product = await this.findOne( id );
+    await this.productRepository.remove( product );
+  }
+
+  private handleDbException( error: any ) {
+    if ( error.code === '23505' )
+      throw new BadRequestException( error.detail );
+    this.logger.error( error );
+    throw new InternalServerErrorException( `Unexpected error, check server logs.` );
+  }
+}
