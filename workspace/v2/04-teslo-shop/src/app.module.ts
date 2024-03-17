af27ca36d1afb3f1381b653a421f5010a8890c1e
Leandro C. Tomassini <leandroctomassini@gmail.com>
2024-03-17 18:35:23 -0300

    Seccion 16

--- workspace/v2/04-teslo-shop/src/app.module.ts
+++ workspace/v2/04-teslo-shop/src/app.module.ts
@@ -12,24 +12,30 @@ import { FilesModule } from './files/files.module';
 import { AuthModule } from './auth/auth.module';
 import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
-@Module({
+@Module( {
   imports: [
     ConfigModule.forRoot(),
 
-    TypeOrmModule.forRoot({
+    TypeOrmModule.forRoot( {
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+          ? { rejectUnauthorized: false }
+          : null
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,      
+      password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
-    }),
+    } ),
 
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname,'..','public'), 
-    }),
+    ServeStaticModule.forRoot( {
+      rootPath: join( __dirname, '..', 'public' ),
+    } ),
 
     ProductsModule,
 
@@ -44,5 +50,5 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
     MessagesWsModule,
 
   ],
-})
-export class AppModule {}
+} )
+export class AppModule { }
--- workspace/v2/04-teslo-shop/src/main.ts
+++ workspace/v2/04-teslo-shop/src/main.ts
@@ -4,29 +4,32 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const logger = new Logger('Bootstrap');
 
-  app.setGlobalPrefix('api');
+async function bootstrap() {
+  const app = await NestFactory.create( AppModule );
+  app.enableCors();
+  const logger = new Logger( 'Bootstrap' );
+
+  app.setGlobalPrefix( 'api' );
 
   app.useGlobalPipes(
-    new ValidationPipe({
+    new ValidationPipe( {
       whitelist: true,
       forbidNonWhitelisted: true,
-    })
+    } )
   );
 
   const config = new DocumentBuilder()
-    .setTitle('Teslo RESTFul API')
-    .setDescription('Teslo shop endpoints')
-    .setVersion('1.0')
+    .setTitle( 'Teslo RESTFul API' )
+    .setDescription( 'Teslo shop endpoints' )
+    .setVersion( '1.0' )
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument( app, config );
+
+  SwaggerModule.setup( 'api', app, document );
 
 
-  await app.listen(process.env.PORT);
-  logger.log(`App running on port ${ process.env.PORT }`);
+  await app.listen( process.env.PORT );
+  logger.log( `App running on port ${ process.env.PORT }` );
 }
 bootstrap();
